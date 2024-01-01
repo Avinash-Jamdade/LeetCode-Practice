f6d4be92b5845e7cdc42354b1d67af4bff2ca765
Avinash-Jamdade <154262517+Avinash-Jamdade@users.noreply.github.com>
2024-01-01 22:20:09 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (6.55%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+       double sqr = Math. sqrt(num);
+       int val = (int) sqr;
+    if((val*val)==num)
+    {
+        return true;
+    }
+    return false;
+        
+    }
+}
