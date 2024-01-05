720b77a450a7868cd4f94ace972245161dc4c8f8
Avinash-Jamdade <154262517+Avinash-Jamdade@users.noreply.github.com>
2024-01-05 22:45:19 +0530

    Time: 1 ms (74.27%), Space: 40.6 MB (22.28%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,21 +1,23 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n<1){
-            return false;
+       return Math.log10(n)/Math.log10(4) % 1==0;
+//         if(n<1){
+//             return false;
             
-        }
-        while(n>1){
-           if(n % 4 !=0){
-               return false;
-           }
-            n=n/4;
-        }
-        if(n==1){
-            return true; 
-        }
-        else{
-             return false;
-        }
+//         }
+//         while(n>1){
+//            if(n % 4 !=0){
+//                return false;
+//            }
+//             n=n/4;
+//         }
+//         if(n==1){
+//             return true; 
+//         }
+//         else{
+//              return false;
+//         }
         
+//     }
     }
 }
