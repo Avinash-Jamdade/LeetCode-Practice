dd3a8bb7aba005cfe89dffc86b10c862e4803788
Avinash-Jamdade <154262517+Avinash-Jamdade@users.noreply.github.com>
2024-01-21 11:32:53 +0530

    Time: 9 ms (80.51%), Space: 45.7 MB (6.46%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int a=g.length;
+        int b=s.length;
+        
+        int i=0,j=0;
+        while(i<a && j<b){
+            if(g[i]<=s[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
