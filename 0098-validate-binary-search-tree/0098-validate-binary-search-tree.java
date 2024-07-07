70373d0564d4e5ec65aab4be64962eed2af24cd6
Avinash-Jamdade <154262517+Avinash-Jamdade@users.noreply.github.com>
2024-07-07 12:48:43 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (22.20%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,14 +13,33 @@
  *     }
  * }
  */
+// public class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+//     }
+    
+//     public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+//         if (root == null) return true;
+//         if (root.val >= maxVal || root.val <= minVal) return false;
+//         return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+//     }
+// }
 public class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return helper(root,null,null);
     }
-    
-    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
-        if (root == null) return true;
-        if (root.val >= maxVal || root.val <= minVal) return false;
-        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    public boolean helper(TreeNode node, Integer low,Integer high){
+        if(node == null){
+            return true;
+        }
+        if(low != null && node.val <= low){
+            return false;
+        }
+        if(high != null && node.val >= high){
+            return false;
+        }
+        boolean leftTree=helper(node.left,low,node.val);
+        boolean rightTree=helper(node.right,node.val,high);
+        return leftTree && rightTree;
     }
 }
