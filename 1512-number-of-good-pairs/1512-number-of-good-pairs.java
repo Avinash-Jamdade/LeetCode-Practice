0ef273f8f01f86f37613efdf8c4682fa3fd5599f
Avinash-Jamdade <154262517+Avinash-Jamdade@users.noreply.github.com>
2024-07-28 09:31:26 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (26.99%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -3,12 +3,8 @@ class Solution {
         int res=0;
         int countArray[]=new int[101];
         for(int num:nums){
-            countArray[num]++;
-        }
-        for(int count:countArray){
-            count=(count*(count -1))/2;
-            res +=count;
+            res += countArray[num]++;
         }
         return res;
-    }
+    }   
 }
