cee331df2b0982234f88c382cca7b9392f70d2cd
Avinash-Jamdade <154262517+Avinash-Jamdade@users.noreply.github.com>
2024-08-05 15:35:40 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (81.51%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,6 +1,11 @@
 class Solution {
     public String defangIPaddr(String address) {
-        StringBuilder replace=new StringBuilder();
+      String replaceaddress=address.replace(".","[.]");
+        return replaceaddress;
+    }
+}
+/* 
+  StringBuilder replace=new StringBuilder();
         for(char a:address.toCharArray()){
             if(a=='.'){
                 replace.append("[.]");
@@ -10,5 +15,4 @@ class Solution {
             }
         }
         return replace.toString();
-    }
-}
+*/
