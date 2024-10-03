788d3e2650797ded550d42ed10ca3cd2b803385a
Avinash-Jamdade <154262517+Avinash-Jamdade@users.noreply.github.com>
2024-10-03 20:33:16 +0530

    Time: 26 ms (77.28%), Space: 67.3 MB (31.81%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,30 +1,59 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) { 
+        HashSet<Integer> a= new HashSet<Integer>();
+        int count=0;
+        int len=Integer.MIN_VALUE;
+        
+        if(nums.length==0)
+        {
             return 0;
         }
-
-        HashSet<Integer> numSet = new HashSet<>();
         for (int num : nums) {
-            numSet.add(num);
-        }
-
-        int longest = 0;
-        for (int num : nums) {
-         
-            if (!numSet.contains(num - 1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (numSet.contains(currentNum + 1)) {
-                    currentNum += 1;
-                    currentStreak += 1;
-                }
-
-                longest = Math.max(longest, currentStreak);
+            a.add(num); 
+        }for(int it:a)
+        {
+            if(!a.contains(it-1))
+            {
+                count=1;
+                 int x=it;
+                 while(a.contains(x+1))
+                 {
+                    x=x+1;
+                    count+=1;
+                 }
+                 len=Math.max(len,count);
             }
-        }
-
-        return longest;
+        }       
+        return len;
     }
 }
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         if (nums.length == 0) { 
+//             return 0;
+//         }
+
+//         HashSet<Integer> numSet = new HashSet<>();
+//         for (int num : nums) {
+//             numSet.add(num);
+//         }
+
+//         int longest = 0;
+//         for (int num : nums) {
+         
+//             if (!numSet.contains(num - 1)) {
+//                 int currentNum = num;
+//                 int currentStreak = 1;
+
+//                 while (numSet.contains(currentNum + 1)) {
+//                     currentNum += 1;
+//                     currentStreak += 1;
+//                 }
+
+//                 longest = Math.max(longest, currentStreak);
+//             }
+//         }
+
+//         return longest;
+//     }
+// }
