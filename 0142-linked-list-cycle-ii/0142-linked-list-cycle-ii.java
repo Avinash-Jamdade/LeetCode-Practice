c70601c5160173634b72fc4919c05c9f7f28cccf
Avinash <154262517+Avinash-Jamdade@users.noreply.github.com>
2025-12-06 19:12:16 +0530

    Time: 0 ms (100%), Space: 46.5 MB (34.66%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,53 +10,23 @@
  * }
  */
 public class Solution {
-    public int lengthCycle(ListNode head){
-        ListNode fast=head;
-        ListNode slow=head;
-        while(fast != null && fast.next !=null){
-            fast=fast.next.next;
-            slow=slow.next;
-                if(fast == slow){
-                    ListNode temp=slow;
-                    int length=0;
-            do{
-                temp=temp.next;
-                length++;
-            }
-            while(temp != slow);
-                return length;
-        }
-        }
-        return 0;
-    }
     public ListNode detectCycle(ListNode head) {
-        int length=0;
-        ListNode fast= head;
         ListNode slow=head;
-        while(fast != null && fast.next!= null){
-            fast=fast.next.next;
+        ListNode fast=head;
+
+        while(fast !=null && fast.next != null){
             slow=slow.next;
-               if(fast == slow){
-            length= lengthCycle(slow);
-            break;
+            fast=fast.next.next;
+
+            if(slow==fast){
+                slow=head;
+                while(slow != fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
         }
-        }
-        if(length==0){
-            return null;
-        }
-     ListNode f=head;
-        ListNode s=head;
-        
-        while(length>0){
-            s=s.next;
-            length--;
-        }
-        while(f !=s){
-            f=f.next;
-            s=s.next;
-        }
-        return s;
+        return null;        
     }
-    
-    
 }
